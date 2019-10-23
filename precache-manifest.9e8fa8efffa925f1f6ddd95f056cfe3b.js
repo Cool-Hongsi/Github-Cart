b0d0b4aa1e4eba67cdc43d2875118aec774d652d
Sungjun <hongsi140626@gmail.com>
2019-10-23 03:19:13 -0400

    Updates

---
+++ precache-manifest.9e8fa8efffa925f1f6ddd95f056cfe3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df21449e36e79078e6af04ff20417ef",
+    "url": "/Github-Cart/index.html"
+  },
+  {
+    "revision": "0bfe4aab84aa6ef85c99",
+    "url": "/Github-Cart/static/css/main.4b01e57d.chunk.css"
+  },
+  {
+    "revision": "d366c980d47013fab092",
+    "url": "/Github-Cart/static/js/2.67630923.chunk.js"
+  },
+  {
+    "revision": "0bfe4aab84aa6ef85c99",
+    "url": "/Github-Cart/static/js/main.1c8a6512.chunk.js"
+  },
+  {
+    "revision": "493c13572e3c6910021a",
+    "url": "/Github-Cart/static/js/runtime~main.8b4cec43.js"
+  },
+  {
+    "revision": "095820ebeaaffb88f02d59b144169ac3",
+    "url": "/Github-Cart/static/media/color-design-flora-1166644.095820eb.jpg"
+  }
+]);
